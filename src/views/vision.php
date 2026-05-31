51ab344070e5b6ca6b2e78d867020c982380424c
Ranavar <94916071+NikitaRTN@users.noreply.github.com>
2026-05-31 03:28:26 +0200

    Fix vision page rendering

--- src/views/vision.php
+++ src/views/vision.php
@@ -1,6 +1,4 @@
 <?php
 declare(strict_types=1);
-?>
-<link rel="stylesheet" href="<?php echo e(asset_url('src/assets/laning.css')); ?>">
-<?php
-render_laning_page($match, $radiant_players, $dire_players, $heroes);
+
+render_vision_page($match, $radiant_players, $dire_players, $heroes, $items_by_id);
