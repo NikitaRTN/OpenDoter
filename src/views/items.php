23f218b7ae1a95fcc8d49ffb0c1cd80275fa9685
Ranavar <94916071+NikitaRTN@users.noreply.github.com>
2026-05-31 21:46:02 +0200

    feat(items): тайминги покупок (purchase_log), бэкпак и нейтральный предмет на /items

--- src/views/items.php
+++ src/views/items.php
@@ -2,7 +2,4 @@
 
 declare(strict_types=1);
 
-$players = array_merge($radiant_players, $dire_players);
-render_match_tab_section(match_tab_title('items'), 'предметы игроков', static function () use ($players, $heroes, $items_by_id): void {
-    render_items_match_tab($players, $heroes, $items_by_id);
-});
+render_items_tab($radiant_players, $dire_players, $heroes, $items_by_id);
